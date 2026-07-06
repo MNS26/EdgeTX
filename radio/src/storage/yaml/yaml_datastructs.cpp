929c3242b89a1c5f9ae58bef566b02bd72dc4c20
Noah <53867268+MNS26@users.noreply.github.com>
2026-07-06 20:55:51 +0200

    fix yaml_datastructs file naming for linux

--- radio/src/storage/yaml/yaml_datastructs.cpp
+++ radio/src/storage/yaml/yaml_datastructs.cpp
@@ -85,7 +85,7 @@
    #include "yaml_datastructs_x9dp2019.cpp"
   #endif
 #elif defined(RADIO_LINUX)
- #include "yaml_datastructs_pi.cpp"
+ #include "yaml_datastructs_linux.cpp"
 #else
 #error "Board not supported by YAML storage"
 #endif
